846e8c530fcb2c738e3094d40877f688b90a5da0
karmut <karina.mutafyan@gmail.com>
2023-02-27 17:44:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let counter = 3;
+
+for (let counter = 3; counter >= 30; counter++) {
+  console.log(counter);
+}
+
+//---------------------------
+
+let a = 5;
+
+for (let a = 5; a > -10; a--) {
+  console.log(a);
+}
+//----------------------------
+
+let correkt = 0;
+let number;
+
+while (correkt != number) {
+  number = prompt("Enter your Number");
+}
